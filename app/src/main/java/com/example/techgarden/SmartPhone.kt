f5adeaee2c7f729fbe2b7411d439deb2069e4f2d
ricar <ricardosr4@gmail.com>
2024-04-25 00:03:27 -0400

    ok

--- app/src/main/java/com/example/techgarden/SmartPhone.kt
+++ app/src/main/java/com/example/techgarden/SmartPhone.kt
@@ -1,9 +1,27 @@
 package com.example.techgarden
 
-class SmartPhone(brand: String, model: String, state: State) {
+class SmartPhone(brand: String, model: String, state: State) : Device(brand,model, state){
 
     val dualSim = Boolean
-    val listSmartPhone = mutableListOf<Device>()
+    companion object{
+    val listSmartPhone = mutableListOf<Device>(
+        Computer("acer","intel i5", State.NUEVO),
+        Computer("HP", "AMD Rayzer 5", State.USADO),
+        Tablet("Lenovo", "A8", State.EN_REPARACION),
+        SmartPhone("Samsung", "S22", State.EN_REPARACION)
+
+
+
+    )
+    }
+    fun getListSmartPhone(): String {
+        val stringBuilder = StringBuilder()
+        for (device in listSmartPhone) {
+            stringBuilder.append(device.toString()).append("\n")
+        }
+        return stringBuilder.toString()
+    }
+
 
    /*fun ok3(): Boolean {
         return true
